561be6a44cb72a075380f3755160439a807d563f
othmanoach <othmanoach@gmail.com>
2023-09-19 15:41:56 +0100

    nop

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- give nothing
+  *@head: the stack head
+  *@counter: the line number
+  *Return: nothing
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
