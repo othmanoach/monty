befa0dfcb98ed6c6ec581128e2182ef6315a40bc
othmanoach <othmanoach@gmail.com>
2023-09-20 14:40:14 +0000

    reset errors

--- execute.c
+++ execute.c
@@ -8,7 +8,7 @@
 * Return: nothing
 */
 
-int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
+void execute(char *content, stack_t **stack, unsigned int line_number, FILE *file)
 {
 	instruction_t opst[] = {
 				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
@@ -33,13 +33,11 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 
 	op = strtok(content, " \n\t");
 	if (op && op[0] == '#')
-		return (0);
 	bus.arg = strtok(NULL, " \n\t");
 	while (opst[i].opcode && op)
 	{
 		if (strcmp(op, opst[i].opcode) == 0)
 		{	opst[i].f(stack, counter);
-			return (0);
 		}
 		i++;
 	}
@@ -49,5 +47,5 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 		free(content);
 		free_stack(*stack);
 		exit(EXIT_FAILURE); }
-	return (1);
+
 }
--- monty.h
+++ monty.h
@@ -56,11 +56,7 @@ char  *clean_line(char *content);
 void f_push(stack_t **head, unsigned int number);
 void f_pall(stack_t **head, unsigned int number);
 void f_pint(stack_t **head, unsigned int number);
-/* Function declarations */
-void execute(char *content, stack_t **stack, unsigned int line_number, FILE *file);
-void push(stack_t **stack, int num);
-void pall(stack_t *stack);
-/*int execute(char *content, stack_t **head, unsigned int counter, FILE *file);*/
+int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
 void f_pop(stack_t **head, unsigned int counter);
 void f_swap(stack_t **head, unsigned int counter);
